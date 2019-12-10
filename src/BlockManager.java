b00c9151ce7e59b3a103957b2d0ea7ad94adbc50
Anthony6197 <zmnYTW#24686197>
2019-12-10 02:00:03 -0600

    make the map goes zigzag like （Boustrophedon）

--- src/BlockManager.java
+++ src/BlockManager.java
@@ -7,61 +7,68 @@ public class BlockManager {
     private CanvasWindow canvas;
     private List<Block> blocks;
     private static final int BLOCK_QUANTITY = 36;
-    private final List<String> availableTypes = List.of("Math","Chem");
+    private final List<String> availableTypes = List.of("Math", "Chem");
 
-    public BlockManager(CanvasWindow canvas){
+    public BlockManager(CanvasWindow canvas) {
         this.canvas = canvas;
     }
 
     public void generateBlock() {
         this.blocks = new ArrayList<>();
+
+        List<String> BlockbyType = new ArrayList<>();
+        int rounds = BLOCK_QUANTITY / availableTypes.size();
+        int remainder = BLOCK_QUANTITY % availableTypes.size();
+        for(int i = 0; i < rounds; i++){
+            BlockbyType.addAll(availableTypes);
+        }
+        if(remainder != 0){
+            BlockbyType.addAll(availableTypes.subList(0,remainder));
+        }
+
         double leftEdge = canvas.getWidth() * 0.1;
         double rightEdge = canvas.getWidth() * 0.9;
         double x = leftEdge;
         double y = canvas.getHeight() * 0.1;
+        boolean reverse = false;
         double blockSize = canvas.getWidth() * 0.058;
-
-        List<String> BlockByType = new ArrayList<>();
-        int rounds = BLOCK_QUANTITY / availableTypes.size();
-        int remainder = BLOCK_QUANTITY % availableTypes.size();
-        for(int i = 0; i < rounds; i++){
-            BlockByType.addAll(availableTypes);
-        }
-        if(remainder != 0){
-            BlockByType.addAll(availableTypes.subList(0,remainder));
-        }
 
         for (int i = 0; i < BLOCK_QUANTITY; i++) {
             Block block = new Block(x, y, blockSize, blockSize,i);
-            block.setType(BlockByType.get(i));
+            block.setType(BlockbyType.get(i));
             blocks.add(block);
             canvas.add(block);
 
-            x += blockSize;
-            if (x + blockSize >= rightEdge) {
-                x = leftEdge;
-                y += blockSize;
+                if (x + 2 * blockSize >= rightEdge && !reverse || x - blockSize < leftEdge && reverse) {
+                    reverse = !reverse;
+                    y += blockSize;
+                } else {
+                    if (reverse){
+                        x -= blockSize;
+                    } else {
+                        x += blockSize;
+                    }
+                }
             }
         }
-    }
 
-    public Block getBlock(int index){
-        for(Block block: blocks){
-            if(index == block.getIndex()){
-                return block;
+        public Block getBlock ( int index){
+            for (Block block : blocks) {
+                if (index == block.getIndex()) {
+                    return block;
+                }
             }
+            return null;
         }
-        return null;
-    }
 
-    public List<Block> getPassedBlocks(int index){
-        return blocks.subList(0,index+1);
-    }
+        public List<Block> getPassedBlocks ( int index){
+            return blocks.subList(0, index + 1);
+        }
 
-    public int getBlockQuantity(){
-        return BLOCK_QUANTITY;
-    }
-    // compensate for last time commit
+        public int getBlockQuantity () {
+            return BLOCK_QUANTITY;
+        }
+        // compensate for last time commit
 
 }
 
